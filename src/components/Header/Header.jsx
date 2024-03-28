8127ba12aeba9e8ed5abbce82a3f62badf5f273a
menna-adel-10 <menna10_adel@hotmail.com>
2024-03-28 07:05:12 +0200

    .

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -18,8 +18,8 @@ const Header = () => {
   return (
     <div className="fixed top-0 left-0 w-full bg-white z-10 shadow-md">
       <div className="container mx-auto px-4 flex justify-between items-center py-4">
-        <Link to="/" className="text-primary font-montez text-[25px] md:text-[30px] font-bold tracking-wide">
-           Mimozo
+        <Link to="/" className="text-white font-montez bg-primary p-2 rounded-full text-[25px] md:text-[30px] font-bold tracking-wide">
+           Find
         </Link>
 
         <ul className={`h-[200px] md:h-[50px] z-200 top-0 right-0 bg-opacity-95 bg-blur fixed flex flex-col justify-center items-center text-leading-6 md:static md:flex-row md:space-x-8 ${click ? 'absolute md:static bg-white right-0 top-full w-full md:w-auto md:bg-transparent overflow-y-auto transition duration-300 ease-in-out' : 'hidden lg:flex'}`}>
